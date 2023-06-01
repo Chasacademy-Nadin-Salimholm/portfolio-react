f83d490adf20793b8be8825dea56ee3fe862dffd
Your Name <you@example.com>
2023-06-01 23:03:20 +0200

    contact page updated

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+function About() {
+    return (
+
+        <div>
+            HEJ
+        </div>
+    );
+}
+
+export default About;
